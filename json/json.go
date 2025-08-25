d8200377ec26d4131cd9e4d471afa51f2e54cde5
mel2oo <l977631253@gmail.com>
2025-08-25 14:21:59 +0800

    feat: json序列化限制长度

--- json/json.go
+++ json/json.go
@@ -6,7 +6,17 @@ import (
 	"strings"
 )
 
-func MarshalJsonString(v any) string {
+const maxJSONSize = 50 * 1024
+
+func Marshal(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func Unmarshal(data []byte, v any) error {
+	return json.Unmarshal(data, v)
+}
+
+func MarshalString(v any) string {
 	data, err := json.Marshal(v)
 	if err != nil {
 		return ""
@@ -14,10 +24,18 @@ func MarshalJsonString(v any) string {
 	return string(data)
 }
 
-func MarshalPureJsonString(v any) string {
+func MarshalPureString(v any) string {
 	buffer := &bytes.Buffer{}
 	encoder := json.NewEncoder(buffer)
 	encoder.SetEscapeHTML(false)
 	encoder.Encode(v)
 	return strings.TrimSuffix(buffer.String(), "\n")
+}
+
+func MarshalStringTruncated(v any) string {
+	data := MarshalString(v)
+	if len(data) > maxJSONSize {
+		return data[:maxJSONSize] + "...(truncated)"
+	}
+	return data
 }
