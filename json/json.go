0ce42300c55a96ac39d7a0e32485adff4c8ea06e
mel2oo <l977631253@gmail.com>
2025-09-01 14:27:19 +0800

    feat: json encoder, decoder

--- json/json.go
+++ json/json.go
@@ -3,6 +3,7 @@ package json
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"strings"
 )
 
@@ -14,6 +15,14 @@ func Marshal(v any) ([]byte, error) {
 
 func Unmarshal(data []byte, v any) error {
 	return json.Unmarshal(data, v)
+}
+
+func NewDecoder(r io.Reader) *json.Decoder {
+	return json.NewDecoder(r)
+}
+
+func NewEncoder(w io.Writer) *json.Encoder {
+	return json.NewEncoder(w)
 }
 
 func MarshalString(v any) string {
