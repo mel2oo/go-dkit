e12d767e1ba86e3828a54ac68615e7fa051147aa
mel2oo <l977631253@gmail.com>
2025-11-10 17:37:57 +0800

    update fs

--- fs/fs.go
+++ fs/fs.go
@@ -9,7 +9,7 @@ func Mkdir(path string) error {
 	stat, err := os.Stat(path)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return os.MkdirAll(path, os.ModeDir)
+			return os.MkdirAll(path, os.ModePerm)
 		}
 		return err
 	}
@@ -18,5 +18,26 @@ func Mkdir(path string) error {
 		return fmt.Errorf("path is not a directory: %s", path)
 	}
 
+	return nil
+}
+
+func MkdirIf(path string, uid, gid int) error {
+	stat, err := os.Stat(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+
+		if err := os.MkdirAll(path, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create directory %s: %w", path, err)
+		}
+	}
+
+	if !stat.IsDir() {
+		return fmt.Errorf("path exists but is not a directory: %s", path)
+	}
+
+	os.Chmod(path, os.ModePerm)
+	os.Chown(path, uid, gid)
 	return nil
 }
